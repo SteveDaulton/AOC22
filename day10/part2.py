5b7e44c561c20201de320431366d7e119fe1576d
Steve Daulton <stevedaulton@gmail.com>
2022-12-14 10:21:27 +0000

    Update Day 10 docstring.
    Add some explanation to clarify the question.

--- day10/part2.py
+++ day10/part2.py
@@ -2,6 +2,27 @@
 # -*- coding: utf-8 -*-
 
 """Day 10, part 2.
+
+Much trickier than part 1. The hardest part being understanding the
+question :-)
+
+The 'timer' in this case is the CRT scan which moves one px per cycle
+from left to right. The CRT pixel position starts at the extreme left,
+then moves one pixel (one cycle) at a time for 40 cycles, then restarts
+from the zero (left) position on the next line.
+
+The centre of the 'sprite' position is the value of the 'Register X',
+and extends 1 px left and right of the Register X value.
+
+The Register X' value is simply the accumulated values (sum of) the
+'addx' commands plus one (the starting value).
+
+A pixel is '#' when the CRT pixel position coincides with the 'sprite'
+position; that is, the Register X value +/- 1.
+
+Off-by-one issues are intended by the evil Question Master (only joking
+Eric, we love you really). Use either 'pixel position' (zero indexed)
+OR 'cycle count' (one indexed) and forget the other.
 """
 
 
